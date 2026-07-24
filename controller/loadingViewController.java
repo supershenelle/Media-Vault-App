fa3e096d140f183de114bc6e896f6f497210fb0f
shenelle <shenellenono@gmail.com>
2026-07-24 17:18:15 +0000

    huhuhuhu ayaw nya tlg wait
    This reverts commit 2721ca751721181abbe2536cbe3bcd1e2e4b00c4.

--- controller/loadingViewController.java
+++ controller/loadingViewController.java
@@ -1,12 +1,44 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+import java.net.URL;
 
+//TO BE FIXED
 public class loadingViewController {
 
+    @FXML private MediaView mediaView;
+    private MediaPlayer mediaPlayer;
+
     public void startLoading(Runnable onFinished) {
-        if (onFinished != null) {
-            onFinished.run();
+        // 1. Get video path from resources
+        URL videoUrl = getClass().getResource("/video/loading.mp4");
+
+        if (videoUrl == null) {
+            System.err.println("Loading video not found in /video/loading.mp4!");
+            if (onFinished != null) onFinished.run();
+            return;
         }
+
+        Media media = new Media(videoUrl.toExternalForm());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+
+        // 2. Play video and cap at 2 seconds (or 4 seconds as you have set)
+        mediaPlayer.setStopTime(Duration.seconds(2));
+
+        // 3. When finished, clean up media and trigger SceneController callback
+        mediaPlayer.setOnEndOfMedia(() -> {
+            mediaPlayer.stop();
+            mediaPlayer.dispose();
+            if (onFinished != null) {
+                onFinished.run();
+            }
+        });
+
+        mediaPlayer.play();
     }
 }
