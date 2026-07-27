5f1f7ab7bed7c527fa8c5c4d6048fc687c3acf89
MLDG <main.mldg@gmail.com>
2026-07-27 21:06:24 +0800

    implement logout

--- controller/mainViewController.java
+++ controller/mainViewController.java
@@ -19,6 +19,7 @@ public class mainViewController {
     @FXML private Label displayNameLabel;
     @FXML private Label bioLabel;
     @FXML private Label errorLabel;
+    private SceneController sceneController;
     // @FXML private ListView<Media> library;
 
     /*
@@ -39,8 +40,9 @@ public class mainViewController {
 
     private Profile profile;
 
-    public void init(Profile profile, Stage stage) {
+    public void init(Profile profile, Stage stage, SceneController sceneController) {
         this.profile = profile;
+        this.sceneController = sceneController;
         // Library lib = profile.getLibrary();
 
         usernameLabel.setText("Username: @" + profile.getUsername());
@@ -381,7 +383,7 @@ public class mainViewController {
     }
 
     public void handleLogout() {
-
+        sceneController.showLogin();
     }
 
     
