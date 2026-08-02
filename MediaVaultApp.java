236d7a1607f34ebd46b0e8115ab00e6212d1bd36
supershenelle <shenellenono@gmail.com>
2026-08-02 19:39:47 +0800

    fixed font display for app

--- MediaVaultApp.java
+++ MediaVaultApp.java
@@ -1,6 +1,7 @@
 // MediaVaultApp.java
 import controller.SceneController;
 import javafx.application.Application;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import model.Profile;
 
@@ -12,6 +13,13 @@ public class MediaVaultApp extends Application {
     public void start(Stage primaryStage) {
         List<Profile> profiles = new ArrayList<>();
         // profiles.add(new Profile("shen", "Shen", "bio")); // temp, later loaded from file
+        Font.loadFont(getClass().getResourceAsStream("/css/highcruiser.otf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixel1.TTF"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixel2.TTF"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixel3.TTF"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixel4.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixelmix.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("/css/pixelmix_bold.ttf"), 12);
 
         primaryStage.centerOnScreen(); // para magdisplay lang sa center ng screen m
         primaryStage.setMinWidth(800); // para di masyado maliit
